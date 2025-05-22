306dd282b2f23c45960a595b8418fb3cb1fc93b2
Milad Mansouri <Milad.Msrii@Gmail.com>
2025-05-22 18:09:13 +0330

    complete header and navbar menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,50 @@ window.addEventListener('load', () => {
 });
 
 /*•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•*/
+
+
+
+
+const addEventOnElements = (elements, eventType, callback) => {
+  for (let i = 0 ; i < elements.length ; i++) {
+    elements[i].addEventListener(eventType, callback);
+  };
+};
+
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+
+const toggleNavbar = () => {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElements(navTogglers, 'click', toggleNavbar);
+
+
+
+const header = document.querySelector("[data-header]");
+let lastScrollpos = 0;
+
+const hideHeader = () => {
+  const isScrollButtom = lastScrollpos < window.scrollY;
+
+  if (isScrollButtom) {
+    header.classList.add("hide")
+  } else {
+    header.classList.remove("hide")
+  }
+
+  lastScrollpos = window.scrollY
+};
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= 50) {
+    header.classList.add('active');
+    hideHeader();
+  } else {
+    header.classList.remove('active');
+  };
+});
